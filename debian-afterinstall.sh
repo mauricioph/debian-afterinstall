9478d30afaecb483796b1248152634e9086a1f95
mauricioph <jesusmlmph@gmail.com>
2019-01-28 23:45:27 +0000

    Update debian-afterinstall.sh
    Added lists to apt

--- debian-afterinstall.sh
+++ debian-afterinstall.sh
@@ -18,6 +18,23 @@ mv /tmp/.source.list /etc/apt/source.list
 else echo "already non-free and contrib repo installed"
 fi
 
+for i in *.list
+do if [ ! -f /etc/apt/source.list.d/${i} ]
+mv ${i} /etc/apt/source.list.d/
+else echo "${i} is already in place"
+fi
+done
+
+apt install debian-keyring -y
+wget -q https://downloads.plex.tv/plex-keys/PlexSign.key -O - | sudo apt-key add -
+wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+
+
+apt install apt-transport-https
+apt install webmin
 apt update
 apt upgrade -y
 
@@ -28,36 +45,24 @@ echo "Which user should be in sudo?"
 read -s usuario
 usermod -a -G sudo $usuario
 fi
-apt install debian-keyring -y
+
 apt install synaptic apt-xapian-index gdebi gksu -y
 apt install firmware-linux -y
 apt install ttf-freefont ttf-mscorefonts-installer -y
 apt install fonts-noto -y
 apt install qt4-qtconfig -y
 apt install file-roller evince doidon qalculate clementine vlc gimp shotwell gparted gnome-disk-utility libreoffice-writer libreoffice-calc libreoffice-impress -y
-apt install ufw -y
-apt install gufw -y
+apt install ufw gufw -y
 apt install libavcodec-extra ffmpeg -y
 apt install pavucontrol audacity jackd mixxx ncmpdcpp -y
-apt install network-manager-gnome -y
-apt install openssh-server openssl openvpn samba perl phpmyadmin sqlite3 -y
+apt install network-manager-gnome openssh-server openssl openvpn samba perl phpmyadmin sqlite3 -y
 apt install apt-show-versions libio-pty-perl libauthen-pam-perl -y
 apt install git install asciidoc xmlto --no-install-recommends -y
 apt install uuid-dev libattr1-dev zlib1g-dev libacl1-dev e2fslibs-dev libblkid-dev liblzo2-dev -y
-apt install rsync -y
-apt install tg3 -y
-apt install firmware-linux-nonfree -y
-apt install gparted ntfs* testdisk -y
-apt install gdebi -y
+apt install rsync tg3 firmware-linux-nonfree gparted ntfs* testdisk gdebi -y
 apt install libreoffice imagemagick swftools ghostscript pdftohtml ffmpeg  -y
-apt install default-jre -y
-apt install software-properties-common dirmngr -y
-apt install ranger -y
-apt install restartd -y
-apt install imagemagick -y
-apt install smartmontools -y
-apt install fdupes -y
-apt install zbackup -y
+apt install default-jre software-properties-common dirmngr ranger restartd -y
+apt install imagemagick smartmontools fdupes zbackup -y
 apt install libreoffice tesseract-ocr tesseract-ocr-eng clamav imagemagick ghostscript  -y
 
 ufw reset 
