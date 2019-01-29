b4c6af31cee51ad8d4bea9f25ad34354d55be5ef
mauricioph <jesusmlmph@gmail.com>
2019-01-29 00:28:20 +0000

    Update debian-afterinstall.sh

--- debian-afterinstall.sh
+++ debian-afterinstall.sh
@@ -19,23 +19,24 @@ else echo "already non-free and contrib repo installed"
 fi
 
 for i in *.list
-do if [ ! -f /etc/apt/source.list.d/${i} ]
-mv ${i} /etc/apt/source.list.d/
+do if [ ! -f /etc/apt/sources.list.d/${i} ]
+mv ${i} /etc/apt/sources.list.d/
 else echo "${i} is already in place"
 fi
 done
 
 apt install debian-keyring -y
-wget -q https://downloads.plex.tv/plex-keys/PlexSign.key -O - | sudo apt-key add -
-wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-wget http://www.webmin.com/jcameron-key.asc
-apt-key add jcameron-key.asc
+
+wget -q -O - https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add -
+wget -q -O - http://download.videolan.org/pub/debian/videolan-apt.asc | apt-key add -
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
+wget -q -O - http://www.webmin.com/jcameron-key.asc | apt-key add -
 
 
 apt install apt-transport-https
 apt install webmin
 apt update
+apt list --upgradable
 apt upgrade -y
 
 superuserdo=$(which sudo)
@@ -51,15 +52,15 @@ apt install firmware-linux -y
 apt install ttf-freefont ttf-mscorefonts-installer -y
 apt install fonts-noto -y
 apt install qt4-qtconfig -y
-apt install file-roller evince doidon qalculate clementine vlc gimp shotwell gparted gnome-disk-utility libreoffice-writer libreoffice-calc libreoffice-impress -y
-apt install ufw gufw -y
+apt install file-roller evince qalculate clementine vlc gimp shotwell gparted gnome-disk-utility libreoffice-writer libreoffice-calc libreoffice-impress -y
+apt install ufw -y
 apt install libavcodec-extra ffmpeg -y
 apt install pavucontrol audacity jackd mixxx ncmpdcpp -y
 apt install network-manager-gnome openssh-server openssl openvpn samba perl phpmyadmin sqlite3 -y
 apt install apt-show-versions libio-pty-perl libauthen-pam-perl -y
 apt install git install asciidoc xmlto --no-install-recommends -y
 apt install uuid-dev libattr1-dev zlib1g-dev libacl1-dev e2fslibs-dev libblkid-dev liblzo2-dev -y
-apt install rsync tg3 firmware-linux-nonfree gparted ntfs* testdisk gdebi -y
+apt install rsync firmware-linux-nonfree gparted ntfs* testdisk gdebi -y
 apt install libreoffice imagemagick swftools ghostscript pdftohtml ffmpeg  -y
 apt install default-jre software-properties-common dirmngr ranger restartd -y
 apt install imagemagick smartmontools fdupes zbackup -y
