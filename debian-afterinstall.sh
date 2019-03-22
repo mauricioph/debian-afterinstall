6e652ba468c391894e782bf9f01f08a25050353e
mauricioph <jesusmlmph@gmail.com>
2019-03-22 13:14:29 +0000

    Update debian-afterinstall.sh

--- debian-afterinstall.sh
+++ debian-afterinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Debian after fresh install created for Mauricio at my macbook pro 8.1 (2011)
-# (c)2019 Copyleft
+#
+#
 #
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -58,41 +58,29 @@ echo "Which user should be in sudo?"
 read -s usuario
 usermod -a -G sudo $usuario
 
+echo "Installing systems apps"
+apt install synaptic apt-xapian-index gdebi gksu apt-show-versions libio-pty-perl libauthen-pam-perl asciidoc xmlto uuid-dev libattr1-dev e2fsprogs f2fs-tools hfsutils hfsprogs jfsutils reiser4progs xfsprogs xfsdump lm-sensors upower zlib1g-dev libacl1-dev e2fslibs-dev libblkid-dev liblzo2-dev macfanctld unrar htop rofi i3blocks feh compton unclutter nbtscan nmap i3lock zathura suckless surf puddletag sonata alarm-clock-applet lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker -y
 
-apt install synaptic apt-xapian-index gdebi gksu -y
-apt install firmware-linux -y
+echo "Installing non-free system apps"
+apt install default-jre smartmontools fdupes zbackup software-properties-common dirmngr ranger restartd firmware-linux-nonfree gparted ntfs* testdisk gdebi firmware-linux -y
 
-apt install fonts-font-awesome ttf-freefont ttf-mscorefonts-installer -y
-apt install fonts-noto -y
-apt install qt4-qtconfig -y
+echo "Installing base for i3-gaps"
+apt install gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev
 
-apt install file-roller evince qalculate clementine vlc gimp shotwell gparted gnome-disk-utility libreoffice-writer libreoffice-calc libreoffice-impress -y
-apt install ufw -y
-apt install libavcodec-extra ffmpeg -y
-apt install pavucontrol audacity jackd mixxx ncmpdcpp -y
-apt install network-manager-gnome openssh-server openssl openvpn samba perl phpmyadmin sqlite3 -y
-apt install apt-show-versions libio-pty-perl libauthen-pam-perl -y
-apt install git install asciidoc xmlto --no-install-recommends -y
-apt install uuid-dev libattr1-dev zlib1g-dev libacl1-dev e2fslibs-dev libblkid-dev liblzo2-dev -y
-apt install rsync firmware-linux-nonfree gparted ntfs* testdisk gdebi -y
-apt install libreoffice imagemagick swftools ghostscript pdftohtml ffmpeg  -y
-apt install default-jre software-properties-common dirmngr ranger restartd -y
-apt install imagemagick smartmontools fdupes zbackup -y
-apt install libreoffice tesseract-ocr tesseract-ocr-eng clamav imagemagick ghostscript  -y
-sudo apt install lm-sensors
-sudo apt install upower
-sudo apt install mkvtoolnix-gui
-sudo apt install macfanctld
-sudo apt install htop
-sudo apt install rofi i3blocks feh compton unclutter
-sudo apt install unrar
-sudo apt install e2fsprogs f2fs-tools hfsutils hfsprogs jfsutils reiser4progs xfsprogs xfsdump
-sudo apt install nbtscan nmap i3lock zathura suckless surf puddletag sonata alarm-clock-applet lightdm-gtk-greeter lightdm-gtk-greeter-settings light-locker
-sudo apt install mplayer lsdvd libdvdcss aegisub dos2unix mksquashfs xwinwrap obs-studio 
-sudo apt install gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0-dev
+echo "Installing fonts"
+apt install fonts-font-awesome ttf-freefont ttf-mscorefonts-installer fonts-noto qt4-qtconfig -y
 
-sudo apt install murrine
 
+echo "Installing Internet of Things apps"
+apt install rsync network-manager-gnome openssh-server openssl openvpn samba perl phpmyadmin sqlite3 ufw -y
+
+echo "Installing Office apps"
+apt install libreoffice imagemagick swftools ghostscript pdftohtml ffmpeg tesseract-ocr tesseract-ocr-eng clamav imagemagick ghostscript file-roller evince qalculate clementine vlc gimp shotwell gparted gnome-disk-utility libreoffice-writer libreoffice-calc libreoffice-impress -y
+
+echo "Installing Media apps"
+sudo apt install mkvtoolnix-gui mplayer lsdvd libdvdcss aegisub dos2unix mksquashfs xwinwrap obs-studio libavcodec-extra ffmpeg pavucontrol murrine audacity jackd mixxx ncmpdcpp -y
+
+function firewallrules(){
 ufw reset 
 ufw default deny
 ufw allow SSH
@@ -102,10 +90,25 @@ ufw allow "WWW Cache"
 ufw allow Samba
 ufw allow CIFS
 ufw allow webmin
-
 ufw enable
 ufw status
+}
 
+echo "Should I change firewall rules?"
+read firewall
+case ${firewall} in
+	y)
+	firewallrules
+	;;
+	yes)
+	firewallrules
+	;;
+	*)
+	echo "Skipping firewall rules"
+	;;
+esac
+
+echo "Doing git repositories installation on /opt"
 cd /opt
 mkdir -p /opt/repositories
 cd /opt/repositories
@@ -121,5 +124,5 @@ make -j8
 sudo make install
 cd /opt/repositories
 
-
-sudo dpkg-query -l 
+echo "All is installed, here are the programs recently installed"
+dpkg-query -l 
